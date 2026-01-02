2c4437dc0c07ba2cc9df86e9065679151933bc71
PaoloFori <forin.paolo98@gmail.com>
2026-01-02 17:08:54 +0100

    update the names of the directories

--- test/node/logger.cpp
+++ test/node/logger.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
 #include <eigen3/Eigen/Dense>
 #include <rosneuro_msgs/NeuroOutput.h> 
-#include "gmm_cvsa/utils.hpp" 
+#include "gmm_bci/utils.hpp" 
 #include <string>
 #include <vector>
 
